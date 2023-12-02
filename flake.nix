e115777d28a0ed82d0715620d8aa1ea622fac132
Rizqirazkafi <rizqirazkafi56@gmail.com>
2023-12-02 13:10:58 +0700

    added nix-colors

--- flake.nix
+++ flake.nix
@@ -8,9 +8,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-colors.url = "github:misterio77/nix-colors";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
 			lib = nixpkgs.lib;
@@ -24,6 +25,7 @@
 			};
       homeConfigurations."rizqirazkafi" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
+        extraSpecialArgs = { inherit inputs; };
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
