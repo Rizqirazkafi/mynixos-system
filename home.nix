4569cbf3e2d7b63e961d40c4dd8f3268069e4dd7
Rizqirazkafi <rizqirazkafi56@gmail.com>
2023-12-03 23:14:36 +0700

    Move alacritty module to seperate directory

--- home.nix
+++ home.nix
@@ -7,6 +7,7 @@
   home.homeDirectory = "/home/rizqirazkafi";
   imports = [
     inputs.nix-colors.homeManagerModules.default
+    ./features/alacritty.nix
   ];
 
   colorScheme = inputs.nix-colors.colorSchemes.rose-pine;
@@ -42,41 +43,6 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   services.picom.enable = true;
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      colors = with config.colorScheme.colors; {
-        bright = {
-          black = "0x${base00}";
-          blue = "0x${base0D}";
-          cyan = "0x${base0C}";
-          green = "0x${base0B}";
-          magenta = "0x${base0E}";
-          red = "0x${base08}";
-          white = "0x${base06}";
-          yellow = "0x${base09}";
-        };
-        cursor = {
-          cursor = "0x${base06}";
-          text = "0x${base06}";
-        };
-        normal = {
-          black = "0x${base00}";
-          blue = "0x${base0D}";
-          cyan = "0x${base0C}";
-          green = "0x${base0B}";
-          magenta = "0x${base0E}";
-          red = "0x${base08}";
-          white = "0x${base06}";
-          yellow = "0x${base0A}";
-        };
-        primary = {
-          background = "0x${base00}";
-          foreground = "0x${base06}";
-        };
-      };
-    };
-  };
 
 
   programs.bash = {
