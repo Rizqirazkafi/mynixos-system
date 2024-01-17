86ffc0c79b5c9c1b7bafc485d7aa6e50327da766
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-01-17 11:37:49 +0700

    Added more parameters

--- nvim/plugin/flutter-tools.lua
+++ nvim/plugin/flutter-tools.lua
@@ -1,1 +1,7 @@
-require("flutter-tools").setup {}
+require("flutter-tools").setup({
+	flutter_path = "/run/current-system/sw/bin/flutter",
+	root_patterns = { ".git", "pubspec.yaml" },
+	widget_guides = {
+		enabled = true,
+	},
+})
