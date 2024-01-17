4347eccf28f1c69b50b4c62fb789f6eee3ee366b
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-01-17 11:37:34 +0700

    Deleted unused comments

--- nixos/nbfc.nix
+++ nixos/nbfc.nix
@@ -9,19 +9,12 @@ in
 
 {
 
-# If you don't use flakes:
-#environment.systemPackages = with pkgs; [
-#  nix-prefetch-github
-#  nbfc
-#];
-
 # Needed for both flakes and fetchFromGitHub:
 systemd.services.nbfc_service = {
   enable = true;
   description = "NoteBook FanControl service";
   serviceConfig.Type = "simple"; #related upstream config: https://github.com/nbfc-linux/nbfc-linux/blob/main/etc/systemd/system/nbfc_service.service.in
   path = [ pkgs.kmod ];
-  # script = let package = nbfc.packages.${pkgs.system}.nbfc; in "${package}/${command}"; #for fetchFromGitHub
   script = "/home/${myUser}/.nix-profile/${command}"; #for flake
   wantedBy = [ "multi-user.target" ];
 };
