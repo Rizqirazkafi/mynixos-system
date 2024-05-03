eea27b408941825a64878b2317a77a69b547fdcb
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-05-03 21:01:49 +0700

    updated flake and change home-manager release to 23.11 channel

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-colors.url = "github:misterio77/nix-colors";
