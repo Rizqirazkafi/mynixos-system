7c9f177922ff369386a1e162ca18d71b00fed5d2
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-05-16 09:19:50 +0700

    change nbfc config to acer nitro an515-57

--- nixos/nbfc.nix
+++ nixos/nbfc.nix
@@ -1,22 +1,24 @@
-#NoteBook FanControl
-{ config, pkgs, ... }:
-
+# NoteBook FanControl
+{ config, pkgs, inputs, ... }:
 
 let
-  myUser = "rizqirazkafi"; #adjust this to your username
-  command = "bin/nbfc_service --config-file '/etc/nbfc/nbfc.json'";
-in
+  myUser = "rizqirazkafi"; # adjust this to your username
+  command =
+    "bin/nbfc_service --config-file '/home/${myUser}/.config/nbfc.json'";
 
-{
+in {
+  environment.systemPackages = with pkgs;
+    [ inputs.nbfc-linux.packages.x86_64-linux.default ];
 
-# Needed for both flakes and fetchFromGitHub:
-systemd.services.nbfc_service = {
-  enable = true;
-  description = "NoteBook FanControl service";
-  serviceConfig.Type = "simple"; #related upstream config: https://github.com/nbfc-linux/nbfc-linux/blob/main/etc/systemd/system/nbfc_service.service.in
-  path = [ pkgs.kmod ];
-  script = "/home/${myUser}/.nix-profile/${command}"; #for flake
-  wantedBy = [ "multi-user.target" ];
-};
+  # Needed for both flakes and fetchFromGitHub:
+  systemd.services.nbfc_service = {
+    enable = true;
+    description = "NoteBook FanControl service";
+    serviceConfig.Type =
+      "simple"; # related upstream config: https://github.com/nbfc-linux/nbfc-linux/blob/main/etc/systemd/system/nbfc_service.service.in
+    path = [ pkgs.kmod ];
+    script = "${inputs.nbfc-linux.packages.x86_64-linux.default}/${command}";
+    wantedBy = [ "multi-user.target" ];
+  };
 
 }
