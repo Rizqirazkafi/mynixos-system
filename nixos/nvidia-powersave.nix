5222ccb87eb26f9a03c07cb934bb840449b147d4
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-05-26 12:15:50 +0700

    refactor nvidia powersave

--- nixos/nvidia-powersave.nix
+++ nixos/nvidia-powersave.nix
@@ -1,25 +1,30 @@
 { config, lib, pkgs, ... }: {
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.prime = {
+    offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
+    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA 
+    nvidiaBusId = "PCI:1:0:0";
+
+    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA 
+    intelBusId = "PCI:0:2:0";
+  };
   specialisation = {
-    nvidia.configuration = {
-      # Nvidia Configuration 
-      services.xserver.videoDrivers = [ "nvidia" ];
-      hardware.opengl.enable = true;
-
-      # Optionally, you may need to select the appropriate driver version for your specific GPU. 
-      hardware.nvidia.package =
-        config.boot.kernelPackages.nvidiaPackages.stable;
-
-      # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway 
-      hardware.nvidia.modesetting.enable = true;
-
-      hardware.nvidia.prime = {
-        sync.enable = true;
-
-        # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA 
-        nvidiaBusId = "PCI:1:0:0";
-
-        # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA 
-        intelBusId = "PCI:0:2:0";
+    gpu-task.configuration = {
+      hardware.nvidia = {
+        prime.sync.enable = lib.mkForce true;
+        prime.offload = {
+          enable = lib.mkForce false;
+          enableOffloadCmd = lib.mkForce false;
+        };
       };
     };
   };
