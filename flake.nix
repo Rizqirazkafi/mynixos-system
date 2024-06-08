73117a911db2e18b0d83a0a29c137794da3695d8
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-06-08 13:27:08 +0700

    Channel update
    - Update nixpkgs to 24.05
    - added nixpkgs-legacy for luasnip due to bug in 24.05
    - change home-manager channel to unstable
    - change home-manager follow to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -3,11 +3,12 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-legacy.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
-      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     nix-colors.url = "github:misterio77/nix-colors";
     nbfc-linux = {
