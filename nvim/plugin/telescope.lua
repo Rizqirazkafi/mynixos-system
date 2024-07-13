35bbe4f420bca18bc5ddc531692a24b9190ef9b0
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-07-13 15:08:04 +0700

    moved telescope keymap to correct file

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -12,9 +12,3 @@ require("telescope").setup({
 
 require("telescope").load_extension("fzf")
 --require("telescope").load_extension("flutter")
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
