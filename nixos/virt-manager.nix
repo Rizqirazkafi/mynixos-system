842eab7e381abb576375d86aa50608d8cd6382b7
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-07-19 10:47:31 +0700

    change qemu to only use kvm

--- nixos/virt-manager.nix
+++ nixos/virt-manager.nix
@@ -1,13 +1,14 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, pkgs-unstable, ... }: {
+  programs.virt-manager.enable = true;
   virtualisation = {
     libvirtd = {
       enable = true;
       allowedBridges = [ "virbr0" ];
       qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
         swtpm.enable = true;
         ovmf.enable = true;
-        ovmf.packages = [ pkgs.OVMFFull.fd ];
-
       };
     };
     spiceUSBRedirection.enable = true;
