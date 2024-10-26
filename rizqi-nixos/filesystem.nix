93cd80e307850e26a06eb896dbc460fee0b3ba3f
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-10-26 19:08:33 +0700

    fix incorrect partition UUID

--- rizqi-nixos/filesystem.nix
+++ rizqi-nixos/filesystem.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }: {
   fileSystems."/home/rizqirazkafi/secondhdd" = {
-    device = "/dev/disk/by-uuid/c95a382e-a747-4e58-86ae-01856d11285";
+    device = "/dev/disk/by-uuid/c95a382e-a747-4e58-86ae-01856d11285c";
     fsType = "ext4";
   };
 
