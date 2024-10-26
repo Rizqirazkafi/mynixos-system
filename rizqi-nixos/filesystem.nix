198e8a7d94e87cf8f32ebcb7b6e213776a710e2d
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-10-26 18:52:09 +0700

    added second hdd

---
+++ rizqi-nixos/filesystem.nix
@@ -0,0 +1,7 @@
+{ config, lib, pkgs, ... }: {
+  fileSystems."/home/rizqirazkafi/secondhdd" = {
+    device = "/dev/disk/by-uuid/c95a382e-a747-4e58-86ae-01856d11285";
+    fsType = "ext4";
+  };
+
+}
