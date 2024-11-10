026199d836b6567c5a1a98cc3a75ca606aee97c1
Rizqirazkafi <rizqirazkafi56@gmail.com>
2024-11-10 09:49:41 +0700

    assign extra storage for nextcloud

--- rizqi-nixos/filesystem.nix
+++ rizqi-nixos/filesystem.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, ... }: {
-  fileSystems."/home/rizqirazkafi/secondhdd" = {
+  fileSystems."/var/lib/nextcloud" = {
     device = "/dev/disk/by-uuid/c95a382e-a747-4e58-86ae-01856d11285c";
     fsType = "ext4";
   };
