de6cd092c4a99267ffe38341e5371619b75a929a
rizqirazkafi <Rizqirazkafi>
2025-02-11 19:01:27 +0700

    added tabedit

--- nvim/plugin/flutter-tools.lua
+++ nvim/plugin/flutter-tools.lua
@@ -1,4 +1,8 @@
 require("flutter-tools").setup({
 	-- flutter_lookup_cmd = "which flutter",
 	-- root_patterns = { ".git", "pubspec.yaml" },
+	dev_log = {
+		open_cmd = "tabedit",
+		focus_on_open = false,
+	},
 })
