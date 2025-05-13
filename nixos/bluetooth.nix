b686f4f13bc5a26e44ee99aa4b503444d0860759
rizqirazkafi <Rizqirazkafi>
2025-05-13 21:40:01 +0700

    added bluetooth support

---
+++ nixos/bluetooth.nix
@@ -0,0 +1,29 @@
+{ inputs, config, lib, pkgs, ... }: {
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  hardware.bluetooth.settings = {
+    General = {
+      Enable = "Source,Sink,Media,Socker";
+      Experimental = true;
+    };
+  };
+  services.blueman.enable = true;
+
+  # Enable bluetooth control button
+  systemd.user.services.mpris-proxy = {
+    description = "Mpris proxy";
+    after = [ "network.target" "sound.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  };
+
+  hardware.pulseaudio.configFile = pkgs.writeText "default.pa" ''
+    load-module module-bluetooth-policy
+    load-module module-bluetooth-discover
+    ## module fails to load with 
+    ##   module-bluez5-device.c: Failed to get device path from module arguments
+    ##   module.c: Failed to load module "module-bluez5-device" (argument: ""): initialization failed.
+    # load-module module-bluez5-device
+    # load-module module-bluez5-discover
+  '';
+}
