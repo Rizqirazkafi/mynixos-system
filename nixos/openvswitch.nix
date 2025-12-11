113fd4878f23e83576f63b711e540685510f08f2
Rizqirazkafi <rizqirazkafi56@gmail.com>
2025-12-11 10:47:16 +0700

    unused openvswitch

--- nixos/openvswitch.nix
+++ nixos/openvswitch.nix
@@ -1,10 +1,9 @@
 { inputs, config, lib, pkgs, pkgs-unstable, ... }: {
 
-  networking.vswitches.vs1 = {
-    interfaces = [{
-      name = "enp2s0";
-      type = "internal";
-    }];
+  virtualisation.vswitch = {
+    enable = true;
+    package = pkgs.openvswitch;
+
   };
 
 }
