5ba9cd6d8ecc3bda723e254c2855f08c3f135eef
Rizqirazkafi <rizqirazkafi56@gmail.com>
2026-05-31 20:36:13 +0700

    added docker container filesystem

--- nixos/file-system.nix
+++ nixos/file-system.nix
@@ -4,4 +4,10 @@
     fsType = "ntfs-3g";
     options = [ "rw" "uid=1000" ];
   };
+
+  fileSystems."/home/rizqirazkafi/DockerSystem" = {
+    device = "/dev/disk/by-uuid/3ca03ee2-c8e7-4160-8d95-579f1809ba57";
+    fsType = "ext4";
+    options = [ "defaults" "user" "rw" ];
+  };
 }
