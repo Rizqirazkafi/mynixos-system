18c993f38959fddd578f564f1a1a152532c66f49
Rizqirazkafi <rizqirazkafi56@gmail.com>
2026-06-26 01:23:59 +0700

    refactor hl

--- nvim/options.lua
+++ nvim/options.lua
@@ -61,7 +61,7 @@ vim.keymap.set("n", "<leader>ut", "<cmd>UndotreeToggle<CR>")
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.on_yank()
 	end,
 	group = highlight_group,
 	pattern = "*",
