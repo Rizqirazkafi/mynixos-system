3ee543841b069cd343dd2bcf19205a659eff4e2c
Rizqirazkafi <rizqirazkafi56@gmail.com>
2026-06-26 01:23:34 +0700

    update lock file

--- flake.nix
+++ flake.nix
@@ -3,19 +3,19 @@
 
   nixConfig = {
     substituters = [
-      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://cache.nixos.org"
+      # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     ];
   };
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-legacy.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     zen-browser.url = "github:0xc000022070/zen-browser-flake/beta";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # nix-colors.url = "github:misterio77/nix-colors";
@@ -23,30 +23,42 @@
       url = "github:nbfc-linux/nbfc-linux";
       # inputs.nixpkgs.follows = "nixpkgs";
     };
-    stylix.url = "github:danth/stylix/release-25.11";
-    own-texlive.url =
-      "github:nixos/nixpkgs/bf8462aeba50cc753971480f613fbae0747cffc0";
+    stylix.url = "github:danth/stylix/release-26.05";
+    own-texlive.url = "github:nixos/nixpkgs/bf8462aeba50cc753971480f613fbae0747cffc0";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, nbfc-linux, zen-browser, stylix
-    , home-manager, ... }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      nbfc-linux,
+      zen-browser,
+      stylix,
+      home-manager,
+      ...
+    }@inputs:
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
       overlay-nbfc = final: prev: {
         nbfc-linux = prev.nbfc-linux.overrideAttrs (old: {
-          nativeBuildInputs = (old.nativeBuildInputs or [ ])
-            ++ [ final.pkg-config ];
+          nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ final.pkg-config ];
           buildInputs = (old.buildInputs or [ ]) ++ [ final.curl.dev ];
           configureFlags = (old.configureFlags or [ ]) ++ [ "LIBS=-lcurl" ];
         });
       };
       overlay-meson = final: prev: {
-        meson = prev.meson.overrideAttrs (_: { doCheck = false; });
+        meson = prev.meson.overrideAttrs (_: {
+          doCheck = false;
+        });
       };
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [ overlay-nbfc overlay-meson ];
+        overlays = [
+          overlay-nbfc
+          overlay-meson
+        ];
         config = {
           allowUnfree = true;
           android_sdk.accept_license = true;
@@ -57,7 +69,8 @@
         config.allowUnfree = true;
       };
 
-    in {
+    in
+    {
       nixosConfigurations = {
         nixos-laptop = lib.nixosSystem {
           specialArgs = { inherit pkgs-unstable inputs zen-browser; };
